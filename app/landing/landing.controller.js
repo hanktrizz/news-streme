1b6790bc8a375367a6484857bcf0e98f22d4067a
Han K <thihankyaw89@gmail.com>
2018-09-22 23:42:45 +1000

    Staging commit for landing page

--- app/landing/landing.controller.js
+++ app/landing/landing.controller.js
@@ -4,13 +4,19 @@ angular
     .module('newsStreme.landing')
     .controller('landingController', ['NewsApiService', function (NewsApiService) {
         var self = this;
+        self.title = "Trending";
 
         self.headlines = null;
 
-        // var promise = NewsApiService.getNews({ country: "au", category: "business" });
-        // promise.then(function (data) {
-        //     self.headlines = data;
-        // }, function (reason) {
-        //     alert('Failed: ' + reason);
-        // });
+        var promise = NewsApiService.getNews({country: "au", category: "business"});
+        promise.then(function (data) {
+            self.headlines = data;
+            if (data) {
+                // self.headlines = NewsApiService.getArticlesWithImageURL(data);
+            }
+        }, function (reason) {
+            alert('Failed: ' + reason);
+        });
+
+
     }]);
