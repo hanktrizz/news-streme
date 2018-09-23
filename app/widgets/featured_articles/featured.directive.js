d73f38296998d58bb0c607882d2dbb933a45d3c9
Han K <thihankyaw89@gmail.com>
2018-09-24 01:51:28 +1000

    tiny commit after fixing error

--- app/widgets/featured_articles/featured.directive.js
+++ app/widgets/featured_articles/featured.directive.js
@@ -1,21 +1,29 @@
 'use strict';
 
 angular.module('newsStreme')
-    .directive('featuredArticles', [function () {
+    .directive('featuredArticles', ['$document', function ($document) {
         return {
             restrict: 'E',
+            templateUrl: 'app/widgets/featured_articles/featured.template.html',
             link: function (scope, element, attrs) {
-                scope.$watch('$ctrl.headlines', function (newVal, oldVal) {
-                    if (newVal) {
-
+                scope.$watch('$ctrl.headlines', function (datastream, datastale) {
+                    if (datastream) {
+                        var articles = datastream.articles;
+                        if (articles) {
+                            //first handle LHS featured article
+                            var divLHS = angular.element(document.querySelector('#divFeaturedLHS'));
+                            if (divLHS && divLHS.text() === '') {
+                                console.log(articles[0]);
+                                console.log(divLHS);
+                                divLHS.html(articles[0].title);
+                            }
+                        }
                     }
                 });
-                console.log(element.html());
-            },
-            //currently not creating an isolate scope as the directive will be used at one place for now
+            }
+            //currently not creating an isolate scope as the directive is used only at the app landing page
             // scope: {
             //     dataSrc: '='
             // },
-            templateUrl: 'app/widgets/featured_articles/featured.template.html'
         };
     }]);
